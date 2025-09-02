08c972cb5f6b43ec0244e14db9d58ca58156b020
Rens <rens.dimmendaal@gmail.com>
2025-09-02 20:41:13 +0200

    rm print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import uvicorn,os
 from sketchstar.core import *
 
-print(app.routes)
-
 if __name__ == "__main__":
     uvicorn.run('sketchstar.core:app', host="0.0.0.0", port=5001 if os.getenv("PLASH_PRODUCTION") else 5002, reload=True)
